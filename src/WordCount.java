03f763e514e01120ad53d97a1ed229222b92bf92
redealexander9 <redealexander9@gmail.com>
2022-04-30 17:01:24 -0500

    Working on main program

--- src/WordCount.java
+++ src/WordCount.java
@@ -1,39 +1,50 @@
 import immutable.Map;
 import java.util.Scanner;
-import java.util.ArrayList;
+import java.util.Random;
 import java.util.Vector;
-
 public class WordCount {
 
-    public static void main(String args[]){
+    public static void main(String[] args){
         Map m = Map.create();
-
-
-
-
-
-
-        //System.out.println(m.getValue("Mango"));
+        Random r = new Random();
+        int length;
+        int random;
+        Vector randoms = new Vector();
+        Vector words = new Vector();
+        Vector occurs = new Vector();
         Scanner obj = new Scanner(System.in);
-        //ArrayList<String> userInput = new ArrayList<String>();
-        //Vector<String> words = new Vector<String>();
         String temp = obj.nextLine();
         int i = 0;
-
         while(temp.indexOf("quit") != 0){
 
 
 
 
-            String tempArray[] = temp.split(" ");
+            String[] tempArray = temp.split(" ");
+            length = tempArray.length;
             for(String word : tempArray){
                 if(m.getValue(word) != null){
-                    m.setValue(word, m.getValue(word) + 1);
+                    i--;
+                    System.out.println("size1: " + occurs.size());
+                    System.out.println(" i " + (i - 1));
+                    occurs.set(i - 1, (Integer)occurs.get(i - 1) + 1);
+                    m = m.setValue(word, m.getValue(word) + 1);
+
                 }
                 else{
-                    m.setValue(word, i);
-                    i++;
+                    words.add(word);
+                    occurs.add(1);
+                    random = r.nextInt(length);
+                    if(randoms.contains(random)){
+                        random = r.nextInt(length);
+                    }
+                    m = m.setValue(word, random);
+
+                    randoms.add(random);
                 }
+                System.out.println(" i2: " + (i - 1));
+                System.out.println("size2:" + occurs.size());
+                i++;
 
             }
 
@@ -42,6 +53,9 @@ public class WordCount {
 
 
         }
+        System.out.print("/n");
+        System.out.println("Map has size " + m.size() + " with depth " + m.depth());
+
 
     }
 
