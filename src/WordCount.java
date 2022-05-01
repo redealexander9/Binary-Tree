b44d65d211401d84020e7bd3ac46be59ec69f017
redealexander9 <redealexander9@gmail.com>
2022-05-01 09:02:33 -0500

    Working on main program

--- src/WordCount.java
+++ src/WordCount.java
@@ -6,55 +6,30 @@ public class WordCount {
 
     public static void main(String[] args){
         Map m = Map.create();
-        Random r = new Random();
+        //Random r = new Random();
         int length;
-        int random;
-        Vector randoms = new Vector();
+        //int random;
+        //Vector randoms = new Vector();
         Vector words = new Vector();
         Vector occurs = new Vector();
+        //Vector temps = new Vector();
         Scanner obj = new Scanner(System.in);
         String temp = obj.nextLine();
+        String[] arr = temp.split(" ");
         int i = 0;
-        while(temp.indexOf("quit") != 0){
-
-
-
-
-            String[] tempArray = temp.split(" ");
-            length = tempArray.length;
-            for(String word : tempArray){
-                if(m.getValue(word) != null){
-                    i--;
-                    System.out.println("size1: " + occurs.size());
-                    System.out.println(" i " + (i - 1));
-                    occurs.set(i - 1, (Integer)occurs.get(i - 1) + 1);
-                    m = m.setValue(word, m.getValue(word) + 1);
-
-                }
-                else{
-                    words.add(word);
-                    occurs.add(1);
-                    random = r.nextInt(length);
-                    if(randoms.contains(random)){
-                        random = r.nextInt(length);
-                    }
-                    m = m.setValue(word, random);
-
-                    randoms.add(random);
-                }
-                System.out.println(" i2: " + (i - 1));
-                System.out.println("size2:" + occurs.size());
-                i++;
-
-            }
-
+        //System.out.println(temp.indexOf("quit") == -1);
+        while(temp.indexOf("quit") == -1){
+            arr = temp.split(" ");
+            System.out.println(arr);
             temp = obj.nextLine();
-
-
-
         }
-        System.out.print("/n");
-        System.out.println("Map has size " + m.size() + " with depth " + m.depth());
+
+        for(String word : arr){
+            System.out.println(word);
+        }
+
+
+        //System.out.println("Map has size " + m.size() + " with depth " + m.depth());
 
 
     }
